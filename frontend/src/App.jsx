397a789f652f2ef3120d61fb64d99e792565bb25
Luis Antonio <luislituleniantonio@gmail.com>
2026-02-18 16:43:36 +0100

    rodando agora 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        Vite
-        </a>
-        <a href="https://react.dev" target="_blank">
-        React
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and behind developer
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more muito
-      </p>
-    </>
+    <>ola</>
   )
 }
 
